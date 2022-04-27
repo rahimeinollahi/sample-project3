1316d0ed43a714e074db6649c3f4d66e51343856
rahim einollahi <r.einollahi@gamil.com>
2022-04-28 00:24:36 +0430

    Icons added to project

---
+++ src/components/MainNavbar2.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="container mt-3 p-3">
+        <ul class="nav nav-tabs justify-content-center bg-light ">
+            <li class="nav-item" v-for="item in mainNavbarItems" :key="item.id">
+                <router-link class="nav-link text-dark "  :to="item.path">{{item.itemName}}</router-link>
+            </li>
+        </ul>
+        <img src="../../src/assets/body-image.webp" alt="image-body" class="mt-3">
+        
+        <div class="text-start mt-3">
+        
+        <h2>Future technology: 22 ideas about to change our world</h2>
+        <p>The future is coming, and sooner than you think. These emerging technologies will change 
+        the way we live, how we look after our bodies and help us avert a climate disaster.</p>
+        </div>
+
+            <router-link to="/facebook">
+                    <i class="fa fa-facebook mx-2 p-3"></i>
+            </router-link>
+            <i class="fa fa-instagram mx-2 p-3"></i>
+            <i class="fa fa-twitter mx-2 p-3"></i>
+            <i class="fa fa-whatsapp mx-2 p-3"></i>
+            <a href="https://www.youtube.com/results?search_query=tech"><i class="fa fa-youtube mx-2 p-3"></i></a>
+        <div>
+
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'MainNavbar2',
+        data(){
+            return{
+                mainNavbarItems:[
+                    {id:1,itemName:'New',path:"/"},
+                    {id:2,itemName:'Future Tech',path:"/"},
+                    {id:3,itemName:'Nature',path:"/"},
+                    {id:4,itemName:'Human Body',path:"/"},
+                    {id:5,itemName:'Everyday Science',path:"/"},
+                    {id:6,itemName:'Planet Earth',path:"/"},
+                ]
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,15 +1,32 @@
 <template>
   <div>
       <main-navbar />
+      <div class="mt-2 text-start mx-4 row">
+        <div class="col-md"><h3>Science Focus</h3></div>
+
+        <div class="col-md">
+           <form action="">
+            <div class="input-group mb-3">
+              <input type="text" class="form-control " placeholder="Enter Your Search" name='search-text'>
+              <span class="input-group-text">Search</span>
+            </div>
+          </form>
+        </div>
+      </div>
+      <main-navbar-2 />
+
+         
   </div>
 </template>
 
-<script>
+<script> 
   import MainNavbar from './MainNavbar.vue'
+  import MainNavbar2 from './MainNavbar2.vue'
   export default {
     name:"MainPage",
     components:{
-      MainNavbar
+      MainNavbar,
+      MainNavbar2
     }
   }
 </script>
